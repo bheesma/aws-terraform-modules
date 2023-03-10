ff1b634c42898fbd14ee5742138e262eea4963c7
Prakash.vblog@gmail.com <Prakash.vblog@gmail.com>
2023-03-11 06:57:23 +1100

    Apply formatting

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 
 module "mybucket" {
-  source = "./modules/s3"
+  source      = "./modules/s3"
   bucket-name = "hello-2jdk2l3884fh"
 }
 
 module "mybucket2" {
-  source = "./modules/s3"
+  source      = "./modules/s3"
   bucket-name = "hello-kjdk2l3884fh"
 }
